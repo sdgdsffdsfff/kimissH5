f339205c15ea39680f18e6c357cc826082210310
CBS <ericaiyue@gmail.com>
2013-08-09 12:03:18 +0800

    eric branck

--- webBackboneJs/js/views/Kimiss.js
+++ webBackboneJs/js/views/Kimiss.js
@@ -1,4 +1,5 @@
 define(['views/nav/navBar','views/search/Search','views/body/Body','routes/Router'],function(NavBar,SearchView,Body,Router){
+    //eric branch test...
     var _kimiss = Backbone.View.extend({
         navBarTpl: _.template(AppTplMap.navBar),//_.template($('#navBar-tpl').html()),
         loadingTpl:_.template(AppTplMap.loading),//_.template($('#loading-tpl').html()),

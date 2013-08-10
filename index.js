9a48c6c3214c241cb25b3127613d739a68efea89
eric-cai <ericaiyue@gmail.com>
2013-08-10 12:47:46 +0800

    ericaiyue

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var fs = require('fs');
 var express = require('express');
 var data = require('./data');
 var app = express();
+var os = require('os');
+var dns = require('dns');
 var port = 3000;
 app.get('/',function(req,res){
     res.sendfile(__dirname + '/webBackboneJs/index.html');
@@ -11,4 +13,34 @@ app.configure(function(){
     app.use(express.static(__dirname + '/webBackboneJs'));
 });
 app.listen(port);
+showIP();
 console.log('listen on port : '+ port);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function showIP(){
+    var ifaces=os.networkInterfaces();
+    for (var dev in ifaces) {
+        var alias=0;
+        ifaces[dev].forEach(function(details){
+            if (details.family=='IPv4') {
+                console.log(dev+(alias?':'+alias:''),details.address);
+                ++alias;
+            }
+        });
+    }
+}
